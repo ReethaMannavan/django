2abc92e9591a96653f882b67f97a128cacb1b012
ReethaMannavan <108715512+ReethaMannavan@users.noreply.github.com>
2025-08-31 23:28:45 +0300

    Add files via upload

--- 2onlinerecipe/recipe_project/settings.py
+++ 2onlinerecipe/recipe_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG =  "True"
 
 ALLOWED_HOSTS = ['*']
 
