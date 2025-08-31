34033df679bcb30ce3cbb4b3463a00f6bc84f13d
ReethaMannavan <108715512+ReethaMannavan@users.noreply.github.com>
2025-09-01 00:02:16 +0300

    Add files via upload

--- portfolio/portfolio_project/settings.py
+++ portfolio/portfolio_project/settings.py
@@ -204,7 +204,7 @@ USE_TZ = True
 
 # ✅ Static files
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "portfolio" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
